2d48147f3cb1e22bb349162dfb9e456fccf53d03
GenesisOP <genesis.dmop@gmail.com>
2022-04-21 18:37:30 -0500

    Agregando vistas

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,9 +1,11 @@
 <header>
+    <div class = "header">
     <a href="/home">Mercado</a>
-    <input type="search" name="searchEntry" id="searchInput">
-    <a href="/register">Registrarse</a>
-    <a href="/login">Iniciar Sesión</a>
+    <input type="search" name="searchEntry" id="searchInput" class="barra">
+    <a class="headerletter" href="/register">Registrarse</a>
+    <a class="headerletter" href="/login">Iniciar Sesión</a> 
     <div>
         <a href="/categories">Categorías</a>
+    </div>
     </div>
 </header>
